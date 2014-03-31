0f2708d62ed916f9f342317ab30006ab8635f6f3
Itsuki Noda <i.noda@aist.go.jp>
2014-04-01 00:56:18 +0900

    add document for Dia utility.

--- Dia/DiaUml.rb
+++ Dia/DiaUml.rb
@@ -8,7 +8,7 @@ end
 
 #--============================================================
 #++
-## Dia file parser
+# Dia file parser
 class Dia
   module Utility ; end ;
   include Utility ;
@@ -28,7 +28,9 @@ class Dia
   ##
   #----------------------------------------------------
   #++
-  ##
+  # initialize
+  # _srcFile_:: a Dia file path.
+  #           If specified, the file is read at the initializatoin.
   def initialize(srcFile = nil)
     @srcFiles = [] ;
     readFile(srcFile) if(srcFile) ;
@@ -38,7 +40,10 @@ class Dia
 
   #----------------------------------------------------
   #++
-  ##
+  # open and read a Dia file into XML document.
+  # the document is set to +@doc+.
+  # _filename_:: a Dia file
+  # *return*:: a XML document.
   def readFile(filename)
     @srcFiles.push(filename) ;
     open(filename,"r"){|strm|
@@ -48,8 +53,9 @@ class Dia
 
   #----------------------------------------------------
   #++
-  ##
-  def scanEachLayer(&block)
+  # pick up each layer XML element and execute a given block.
+  # the block is passed each layer element.
+  def scanEachLayer(&block) # :yields: layer
     XML::XPath.each(@doc.root,"//dia:layer"){|layer|
       block.call(layer) ;
     }
@@ -57,8 +63,9 @@ class Dia
 
   #----------------------------------------------------
   #++
-  ##
-  def scanEachObject(&block)
+  # pick up each object XML element and execute a given block.
+  # the block is passed each object element.
+  def scanEachObject(&block) # :yields: object
     scanEachLayer{|layer|
       XML::XPath.each(layer,"//dia:object"){|object|
         block.call(object) ;
@@ -68,15 +75,16 @@ class Dia
 
   #----------------------------------------------------
   #++
-  ##
+  # add an Object to the object table.
+  # _object_:: an Object to add.
   def addObject(object)
     addToTable(@objectTable, object) ;
   end
 
   #----------------------------------------------------
   #++
-  ##
-  def eachObject(&block)
+  # pick up each object in the object table and execute a given block
+  def eachObject(&block) # :yields: object
     @objectTable.each{|object|
       block.call(object) ;
     }
@@ -84,7 +92,9 @@ class Dia
 
   #----------------------------------------------------
   #++
-  ##
+  # pink up an object by an given _id_.
+  # _id_:: an ID for the object.
+  # *return*:: the object, or nil if not find.
   def getObject(id) ;
     @objectTable.get(:id, id) ;
   end
@@ -94,18 +104,25 @@ end #class Dia
 class Dia
   #--==================================================
   #++
-  ##
+  ## A collection of Utility for Dia processing
   module Utility
     #--------------------------------
     #++
-    ##
+    # get a "dia:attribute" element with a specifiled "name" attribute.
+    # _node_:: top node to start scan.
+    # _name_:: the value of "name" attribute of searching element.
+    # *return*:: the first XML element.
+
     def getAttributeFromNode(node,name)
       XML::XPath.first(node,"dia:attribute[@name='#{name}']") ;
     end
 
     #--------------------------------
     #++
-    ##
+    # get a "dia:string" value who has a specifield "name".
+    # _node_:: top node to start scan.
+    # _name_:: the value of "name" attribute of searching element.
+    # *return*:: a String value.
     def getAttributeStringFromNode(node,name)
       attrNode = getAttributeFromNode(node,name) ;
       if(attrNode)
@@ -119,7 +136,9 @@ class Dia
 
     #--------------------------------
     #++
-    ##
+    # wrapper for adding an object to a table.
+    # _table_:: an object table
+    # _object_:: an object to add to the table.
     def addToTable(table, object)
       table.push(object) ;
     end
@@ -130,18 +149,22 @@ end # class Dia
 class Dia
   #--==================================================
   #++
-  ##
+  ## a class to handle generic UML object in Dia.
   class Uml < Dia
     #--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     #++
-    ##
+    ## table for UML classes
     attr :classTable, true ;
+    ## table for UML generalization (inherit) relations
     attr :generalizationTable, true ;
+    ## table for UML packages
     attr :packageTable, true ;
+    ## table for UML association (has-a) relations
     attr :associationTable, true ;
     #------------------------------------------
     #++
-    ##
+    # create an Dia UML file object.
+    # _srcFile_:: a Dia UML file.
     def initialize(srcFile)
       super
       @classTable = HashedArray.new([:id,:name]) ;
@@ -154,7 +177,8 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
+    # open a Dia UML file and scan.
+    # _srcFile_:: a file to scan.
     def addFile(srcFile)
       readFile(srcFile) ;
       scan() ;
@@ -162,7 +186,9 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
+    # scan an XML document of an Dia UML.
+    # This analyze Class, Generalization, Large Package and Association.
+    # After the first scan, then scan the second round to link internal references.
     def scan()
       scanEachObject{|object|
         type = object.attribute('type').value
@@ -184,7 +210,7 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
+    # establish internal references.
     def linkReferences()
       eachObject(){|object|
         object.linkReferences(self) ;
@@ -193,7 +219,7 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
+    # scan a UML class and create and register an object for it to the table.
     def addClass(object)
       klass = Klass.new(object) ;
       addObject(klass) ;
@@ -202,7 +228,8 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
+    # scan a UML generalization and create
+    # and register an object for it to the table.
     def addGeneralization(object)
       gen = Generalization.new(object) ;
       addObject(gen) ;
@@ -211,7 +238,8 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
+    # scan a UML large package and create
+    # and register an object for it to the table.
     def addLargePackage(object)
       package = Package.new(object) ;
       addObject(package) ;
@@ -220,7 +248,8 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
+    # scan a UML association and create
+    # and register an object for it to the table.
     def addAssociation(object)
       assoc = Association.new(object) ;
       addObject(assoc) ;
@@ -229,8 +258,8 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
-    def eachClass(&block)
+    # execute a given block for each UML class object.
+    def eachClass(&block) # :yields: klass
       @classTable.each{|klass|
         block.call(klass) ;
       }
@@ -238,8 +267,8 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
-    def eachGeneralization(&block)
+    # execute a give block for each UML generalization object.
+    def eachGeneralization(&block) # :yields: generalization
       @generalizationTable.each{|gen|
         block.call(gen) ;
       }
@@ -247,8 +276,8 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
-    def eachPackage(&block)
+    # execute a given block for each UML package object.
+    def eachPackage(&block) # :yields: package
       @packageTable.each{|package|
         block.call(package) ;
       }
@@ -256,8 +285,8 @@ class Dia
 
     #------------------------------------------
     #++
-    ##
-    def eachAssociation(&block)
+    # execute a given block for each UML association object.
+    def eachAssociation(&block) # :yields: association
       @associationTable.each{|assoc|
         block.call(assoc) ;
       }
@@ -275,16 +304,22 @@ class Dia
       include Utility
       #--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
       #++
-      ##
+      ## link to a source document.
       attr :document, true ;
+      ## XML element for this object.
       attr :xml, true ;
+      ## ID for this object.
       attr :id, true ;
+      ## parent object.
       attr :parent, true ;
+      ## name of the object.
       attr :name, true ;
 
       #--------------------------------
       #++
-      ##
+      # create a Dia UML object.
+      # _object_:: an XML element for this object.
+      # _doc_:: source document.
       def initialize(object,doc = nil)
         @document = doc ;
         @xml = object ;
@@ -294,7 +329,7 @@ class Dia
 
       #--------------------------------
       #++
-      ##
+      # scan XML element.
       def scan()
         scanParent() ;
         scanName() ;
@@ -303,13 +338,15 @@ class Dia
 
       #--------------------------------
       #++
-      ##
-      def scanSub() 
+      # sub rountine of scan.
+      # This is called at the last of +scan+ method.
+      # This is used to specify additional operation for each object class.
+      def scanSub()
       end
 
       #--------------------------------
       #++
-      ##
+      # pick up the name of parent object element in XML.
       def scanParent()
         if(parentInfo = XML::XPath.first(@xml,"dia:childnode"))
           @parent = parentInfo.attribute("parent").value ;
@@ -318,28 +355,32 @@ class Dia
 
       #--------------------------------
       #++
-      ##
+      # pick up the name of the object
       def scanName()
         @name = getAttributeString("name") ;
       end
 
       #--------------------------------
       #++
-      ##
+      # pick up the attribute element inXML
+      # _name_:: the name of the attribute to find.
+      # *return*:: attribute element
       def getAttribute(name)
         getAttributeFromNode(@xml,name) ;
       end
 
       #--------------------------------
       #++
-      ##
+      # pick up the value of the attribute.
+      # _name_:: the name of the attribute to find.
+      # *return*:: A String value.
       def getAttributeString(name)
         getAttributeStringFromNode(@xml, name) ;
       end
 
       #--------------------------------
       #++
-      ##
+      # link internal reference by name.
       def linkReferences(umlDoc)
         if(@parent.is_a?(String))
           pobj = umlDoc.getObject(@parent) ;
@@ -360,18 +401,20 @@ class Dia
   class Uml
     #--========================================
     #++
-    ##
+    ## UML Class object
     class Klass < ObjectBase
       #--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
       #++
-      ##
+      ## comment text.
       attr :comment, true ;
+      ## table of attributes
       attr :umlAttributeTable, true ;
+      ## inheritting classes.
       attr :parentClasses, true ;
 
       #--------------------------------
       #++
-      ##
+      # prepare objects.
       def scanSub()
         @umlAttributeTable = HashedArray.new([:name]) ;
         @comment = getAttributeString("comment") ;
@@ -381,7 +424,7 @@ class Dia
 
       #--------------------------------
       #++
-      ##
+      # scan attributes defined in UML class diagram
       def scanUmlAttributes()
         scanEachComposite{|comp|
           attr = Attribute.new(comp,self) ;
@@ -392,8 +435,8 @@ class Dia
 
       #--------------------------------
       #++
-      ##
-      def scanEachComposite(&block)
+      # pick up each composite in the XML definition and execute a given block.
+      def scanEachComposite(&block) # :yields: composition
         attrsNode = getAttribute('attributes') ;
         if(attrsNode) then
           XML::XPath.each(attrsNode,
@@ -405,14 +448,18 @@ class Dia
 
       #--------------------------------
       #++
-      ##
+      # add an inheritting class.
+      # _parentClass_:: parent class.
       def inheritClass(parentClass)
         @parentClasses.push(parentClass) ;
       end
 
       #--------------------------------
       #++
-      ##
+      # generate a text of Ruby class definition.
+      # _expandp_:: if true, expand definitions of inheritting classes
+      # _indent_:: specify indent string for each line.
+      # *return*:: definition text string.
       def toRubyDef(expandp = false, indent = "")
         tab = "  " ;
         ind0 = "\n" + indent ;
@@ -441,7 +488,11 @@ class Dia
 
       #--------------------------------
       #++
-      ##
+      # generate attribute declaration in class definition.
+      # _expandp_:: if true, expand inheritting classes.
+      # _indent_:: indent string for each line.
+      # _inheritStack_:: stack recored of inherit-hierarchy.
+      # *return*:: definition text string.
       def toRubyDefAttribute(expandp, indent, inheritStack = [])
         defstr = "" ;
         attrList = collectAttributesWithOverride(expandp) ;
@@ -460,10 +511,11 @@ class Dia
 
       #--------------------------------
       #++
-      ## collect attrubutes with override information.
-      # return:: [<attrGroup>, <attrGroup>, ...]
+      # collect attrubutes with override information.
+      # _expandp_:: if true, expand inheritting classes.
+      # _attrList_:: list of grouped attribute by name.
+      # *return*:: [<attrGroup>, <attrGroup>, ...]
       # <attrGroup> :: [<attr>, <attr>, ...] ; attrs. with the same name.
-      #
       def collectAttributesWithOverride(expandp, attrList = [])
         # collect attributes in parent klasses
         if(expandp)
@@ -490,7 +542,10 @@ class Dia
 
       #--------------------------------
       #++
-      ##
+      # check override of attribute definition.
+      # _attribute_:: an Attribute object.
+      # _inheritStack_:: stack trace of ihnerit hierarchy.
+      # *return*:: return overrided Attribute, or nil.
       def checkAttributeOverride(attribute, inheritStack = [])
         inheritStack.each{|pclass|
           overridingAttr = pclass.umlAttributeTable.get(:name, attribute.name) ;
@@ -511,22 +566,30 @@ class Dia
     class Klass
       #--==============================
       #++
-      ##
+      # attribute definition in UML classes
       class Attribute
         include Utility
         #--@@@@@@@@@@@@@@@@@@@@
         #++
+        ## name of the attribute.
         attr :name, true ;
+        ## data type of the attribute
         attr :type, true ;
+        ## default or fixed value o the attribute
         attr :value, true ;
+        ## comments for the attribute
         attr :comment, true ;
 
+        ## class that the attribute belong to.
         attr :klass, true ;
+        ## overrided attribute if the attribute override inherited definition.
         attr :overrideFrom, true ;
         
         #----------------------
         #++
-        ##
+        # create an Attribute.
+        # _compNode_:: composition element in Dia XML.
+        # _klass_:: class object that includes the attribute.
         def initialize(compNode,klass)
           @name = getAttributeStringFromNode(compNode,"name") ;
           @type = getAttributeStringFromNode(compNode,"type") ;
@@ -537,7 +600,9 @@ class Dia
 
         #--------------------------------
         #++
-        ##
+        # generate declaration text of the attribute in Ruby class definition.
+        # _indent_:: indent string for each line.
+        # *return*:: declaration text string.
         def toRubyDef(indent = "")
           defstr = "" ;
           defstr << indent << "\#\# #{@name}:: type: #{@type}" ;
@@ -558,17 +623,18 @@ class Dia
   class Uml
     #--========================================
     #++
-    ##
+    # generalization (inherit) relation between classes.
     class Generalization < ObjectBase
       #--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
       #++
-      ##
+      ## inheriting class
       attr :from, true ;
+      ## inherited class
       attr :to, true ;
 
       #--------------------------------
       #++
-      ##
+      # scan each connection to find +from+ and +to+.
       def scanSub()
         XML::XPath.each(@xml,"dia:connections/dia:connection"){|connection|
           handle = connection.attribute('handle').value() ;
@@ -585,7 +651,8 @@ class Dia
 
       #--------------------------------
       #++
-      ##
+      # link actual class object from its name recorded in +from+ and +to+.
+      # _umlDoc_:: a UML document that the generalization belongs to.
       def linkReferences(umlDoc)
         super(umlDoc) ;
         fromObj = umlDoc.getObject(@from) ;
@@ -613,7 +680,7 @@ class Dia
   class Uml
     #--========================================
     #++
-    ##
+    # package definition in Dia's UML.
     class Package < ObjectBase
       #--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
       #++
@@ -632,7 +699,7 @@ class Dia
   class Uml
     #--========================================
     #++
-    ##
+    # association definition in Dia's UML.
     class Association < ObjectBase
       #--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
       #++
@@ -652,14 +719,17 @@ end # class Dia
 class HashedArray < Array
   #--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   #++
-  ## Table
+  ## a list of keys that are used as indexes of keys.
   attr :keyList, true ;
+  ## a table of object indexed by the object itself.
   attr :selfTable, true ;
+  ## a list of tables. The key type of each table is specified by +keyList+
   attr :tables, true ;
 
   #----------------------------------------------------
   #++
-  ## 初期化
+  # 初期化
+  # _keyList_:: an init. value for @keyList.
   def initialize(keyList = [:id])
     super() ;
     @keyList = keyList ;
@@ -668,7 +738,7 @@ class HashedArray < Array
 
   #----------------------------------------------------
   #++
-  ## ハッシュテーブルを準備
+  # ハッシュテーブルを準備
   def setupTable()
     @selfTable = {} ;
     @tables = {} ;
@@ -679,7 +749,8 @@ class HashedArray < Array
 
   #----------------------------------------------------
   #++
-  ## push 置き換え
+  # push 置き換え
+  # _entry_:: add entry for each table.
   def push(entry)
     r = super(entry) ;
     @selfTable[entry] = entry ;
@@ -691,14 +762,19 @@ class HashedArray < Array
 
   #----------------------------------------------------
   #++
-  ## push 置き換え
+  # 存在チェック
+  # _entry_:: the object to be checked.
+  # *return*:: true if the _entry_ exists in the table. Otherwise, false.
   def include?(entry)
     @selfTable.has_key?(entry) ;
   end
 
   #----------------------------------------------------
   #++
-  ## get
+  # get an object specified by key as keyType
+  # _keyType_:: specify which key type is used.
+  # _key_:: the key value.
+  # *return*:: a specified object.
   def get(keyType, key)
     @tables[keyType][key]
   end
